322b7498fcf637874bf865a968549e651f26fad7
Gurinder Hans <hello@gurinderhans.me>
2015-04-16 12:56:39 -0700

    mapped main APs and it's shadows

--- python_files/parseWiFiData.py
+++ python_files/parseWiFiData.py
@@ -3,113 +3,95 @@ Import required modules
 -----------------------
 """
 
-import sqlite3, difflib
+import sqlite3, difflib, os
 from pprint import pprint
 from datetime import datetime
-# import matplotlib.pyplot as plt
 
-# plot.ly imports
-import plotly.plotly as py
-from plotly.graph_objs import *
+class DataParser:
 
+    # Consts.
+    SSIDS=["SFUNET", "SFUNET-SECURE", "eduroam"]
 
-"""
-Load data, initialize vars, and define functions
-------------------------------------------------
-"""
-
-# consts
-SSIDS=["SFUNET", "SFUNET-SECURE", "eduroam"]
-
-# database vars
-con = sqlite3.connect("wifi_data.db")
-cur = con.cursor()
-
-# converts unix timestamp to human readable date time
-dt = lambda x: datetime.fromtimestamp(x)
-
-# returns the similarity ratio for two lists
-diff = lambda x,y: difflib.SequenceMatcher(None, x, y).ratio()
-
-# fetch data
-cur.execute("select * from apsdata_SFU_BURNABY_AQ_3000_East_Street_1")
-# sanitize the list //  also remove alien SSIDS
-data = [( str(i[1]), str(i[2]), int(i[3]), int(i[4]), int(i[5]) ) for i in cur.fetchall() if i[1] in SSIDS]
-
-# define time variables
-startT, endT = data[0][-1], data[-1][-1]
-totalMillis = (data[-1][-1] - data[0][-1])
-
-# checking my movement w/ respect to time
-times = [i[-1]-startT for i in data]
-
-# traces for the plot.ly map
-traces=[]
+    # converts unix timestamp to a readable date time
+    dt = lambda x: datetime.fromtimestamp(x)
+    # returns the similarity ratio for two lists
+    diff = lambda x,y: difflib.SequenceMatcher(None, x, y).ratio()
 
 
-"""
-The main program: >
--------------------
-"""
+    # init this class for the specified database
+    def __init__(self, database):
+        self.cur = sqlite3.connect(database).cursor()
+
+    # returns the list of all tables in the database
+    def get_data_tables(self):
+        self.cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+        return [str(i[0]) for i in self.cur.fetchall()[1:]]
 
 
-# 1. Split DATA by SSID ---------------------------------------------
+    # get data
+    def set_table(self, tableName):
+        # fetch data from table
+        self.cur.execute("select * from " + tableName)
+        # sanitize the list //  also remove alien SSIDS
+        self.data = [( str(i[1]), str(i[2]), int(i[3]), int(i[4]), int(i[5]) ) for i in self.cur.fetchall() if i[1] in DataParser.SSIDS]
 
-# fill dict with keys
-tmpDict={i:[] for i in SSIDS}
-# fill dict keys with data
-for i in data:
-    tmpDict[i[0]].append(i)
-data = tmpDict
+        # set time vars
+        self.startT, self.endT = self.data[0][-1], self.data[-1][-1]
+        self.totalMillis = (self.data[-1][-1] - self.data[0][-1])
 
-
-# 2. Group DATA by BSSID for each SSID -----------------------------
-
-# create dict to store bssid group dicts
-tmp_data={}
-# split data by bssid
-for i in SSIDS:
-    # grab the data chunk for this ssid
-    Tdata = data[i]
-    # create a tmp dictionary for storing split values for each ssid
-    tmpDict={}
-    # append values to dict where each key is a unique BSSID
-    for j in Tdata:
-        try:
-            tmpDict[j[1]].append(j)
-        except:
-            tmpDict[j[1]] = [j]
-
-    tmp_data[i] = tmpDict
-data = tmp_data
-
-# 3. Graphing the data for APs ---------------------------------------
-
-# create graph data for given ssid
-def addToTrace(ssid):
-    print "graphing:",ssid,"APs:",len(data[ssid])
-    for j in data[ssid]:
-        rssis, xTime = [],[]
-        for k in data[ssid][j]:
-            if k[3]>-80: # some rssi filtering
-                rssis.append(k[3])
-                xTime.append((k[-1]-startT)/1000.0)
-
-        if len(rssis) > 15:
-            # print rssis
-            Ttrace = Scatter( x=xTime, y=rssis )
-            traces.append(Ttrace)
+        # time values that data was recorded at to make sure
+        # we have a constant rate
+        self.times = [i[-1]-self.startT for i in self.data]
 
 
+    # parse data
+    def parseData(self):
 
-for i in SSIDS:
-    addToTrace(i)
+        ### 1. Split DATA by SSID ---------------------------------------------
 
-data = Data(traces)
+        # fill dict with keys
+        tmpDict={i:[] for i in DataParser.SSIDS}
+        # fill dict keys with data
+        for i in self.data:
+            tmpDict[i[0]].append(i)
+        self.data = tmpDict
 
-# data = Data([Scatter(y=times)])
+        ### 2. Group DATA by BSSID for each SSID -----------------------------
 
-unique_url = py.plot(data, filename = 'basic-line')
+        # create dict to store bssid group dicts
+        tmp_data={}
+        # split data by bssid
+        for i in DataParser.SSIDS:
+            # grab the data chunk for this ssid
+            Tdata = self.data[i]
+            # create a tmp dictionary for storing split values for each ssid
+            tmpDict={}
+            # append values to dict where each key is a unique BSSID
+            for j in Tdata:
+                if j[3] > -80:
+                    try:
+                        tmpDict[j[1]].append(j)
+                    except:
+                        tmpDict[j[1]] = [j]
+
+            # remove dict key if it has less than 15 items in it's array
+            for k, v in tmpDict.items():
+                if len([m[3] for m in v]) < 15:
+                    del tmpDict[k]
+
+            tmp_data[i] = tmpDict
+
+        self.data = tmp_data
+
+    def graphData(self):
+        traces=[]
+        for i in self.data:
+            for j in self.data[i]:
+                rssis = [k[3] for k in self.data[i][j]]
+                xTimes = [k[-1] for k in self.data[i][j]]
+                traces.append((xTimes, rssis))
+        return traces
+
 
 
 """
--- webapp/js/main.js
+++ webapp/js/main.js
@@ -18,28 +18,64 @@ var customMapOptions = {
   },
 
   tileSize: new google.maps.Size(TILE_SIZE, TILE_SIZE),
-  maxZoom: 6,
+  maxZoom: 16,
   minZoom: 1,
   name: MAP_ID // this is displayed on the top right corner button
 }
 
-function plotData(data) {
+function drawForward(a){
+  var data = a[0]
+  var startT = a[1]
+  var endT = a[2]
+  var totalSeconds = a[3]/1000
+  var scaleFactor = AQ_SIZE/totalSeconds
+
   for (i in data) {
-    for (j in data[i]) {
-      var dataRow = data[i][j]
-      var point = new google.maps.Point(196,60) // start at AQ, that's its top-left corner
+    var ssidPointsCount = Object.keys(data[i]).length
+    for (j in data[i]){
+      var apArr = data[i][j]
+      var point = new google.maps.Point(196,60) // AQ top right
+      var thisLength = ((endT - apArr[0][4])/1000 * scaleFactor)
+      point.y += thisLength
+      MapTools.addMarker(MapProj.fromPointToLatLng(point), "images/routerdot.png", apArr[0][1])
 
-      // the magic formule
-      point.y += ( parseInt(j) * ( AQ_SIZE / (data[i].length - 1.03) ) )
-
-      dataRow.push(point)
-      // console.log(dataRow)
-
-      MapTools.addMarker(MapProj.fromPointToLatLng(point), "images/routerdot.png", dataRow[2])
+      // draw it's shadows
+      apArr.forEach(function(el,i) {
+        point.y = 60
+        var thisLength = ((endT-el[4])/1000)*scaleFactor
+        point.y += thisLength
+        MapTools.addMarker(MapProj.fromPointToLatLng(point), "images/Red-Circle.png", el[1])
+      })
     }
   }
 }
 
+function drawBackward(b){
+  var data = b[0]
+  var startT = b[1]
+  var endT = b[2]
+  var totalSeconds = b[3]/1000
+  var scaleFactor = AQ_SIZE/totalSeconds
+
+  for (i in data) {
+    var ssidPointsCount = Object.keys(data[i]).length
+    for (j in data[i]){
+      var apArr = data[i][j]
+      var point = new google.maps.Point(196,60) // AQ top right
+      var thisLength = AQ_SIZE - ((endT - apArr[0][4])/1000 * scaleFactor)
+      point.y += thisLength
+      MapTools.addMarker(MapProj.fromPointToLatLng(point), "images/routerdot.png", apArr[0][1])
+
+      // draw it's shadows
+      apArr.forEach(function(el,i) {
+        point.y = 60
+        var thisLength = AQ_SIZE - (((endT-el[4])/1000)*scaleFactor)
+        point.y += thisLength
+        MapTools.addMarker(MapProj.fromPointToLatLng(point), "images/Red-Circle.png", el[1])
+      })
+    }
+  }
+}
 function initialize() {
   MapProj = new MercatorProjection();
 
@@ -58,23 +94,15 @@ function initialize() {
   Map.mapTypes.set(MAP_ID, new google.maps.ImageMapType(customMapOptions));
   Map.setMapTypeId(MAP_ID); // can control floor levels with this
 
-  $.post(SERVER_URL, function(r) {
-    for (i in r) {
-      if (i != "apsdata_AQ_East_M_VR") continue
+  $.post(SERVER_URL, {'tables': ['_1', '_R_1']}, function(r) {
+    console.log(r)
+    var a = r[0]
+    var b = r[1]
 
-      // east side
-      var seper = MapTools.seperateByKeys(r[i], SSIDS, 1)
-      plotData(seper)
+    drawForward(a)
+    drawBackward(b)
 
-      var joined=[]
-      for (i in seper) {
-        for (j in seper[i]) {
-          joined.push(seper[i][j])
-        }
-      }
 
-      console.log(joined)
-    }
   })
 }
 
