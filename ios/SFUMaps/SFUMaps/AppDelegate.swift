afd4619858e6a6c4004c855a70a6cdc8a48c4c4d
nbosco <nbosco182@gmail.com>
2015-05-26 10:56:31 -0700

    google API frameworks installed TODO:UI for the map

--- ios/SFUMaps/SFUMaps/AppDelegate.swift
+++ ios/SFUMaps/SFUMaps/AppDelegate.swift
@@ -8,16 +8,30 @@
 
 import UIKit
 
+
+class DemoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var camera = GMSCameraPosition.cameraWithLatitude(37.78318,
+            longitude:-122.40374, zoom:18)
+        
+        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
+        self.view = mapView
+    }
+}
+
 @UIApplicationMain
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
   var window: UIWindow?
   // 1
-  //let googleMapsApiKey = "AIzaSyDg7dpNx9WdiJVOHlJKkpk_wH6Gk4ktwOw"
+  let googleMapsApiKey = "AIzaSyAESyQ3QOxfiLHfNViGoQx5S2XzIg5juCg"
   
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    // 2
-    //GMSServices.provideAPIKey(googleMapsApiKey)
+    //2
+    GMSServices.provideAPIKey(googleMapsApiKey)
     return true
   }
 }
