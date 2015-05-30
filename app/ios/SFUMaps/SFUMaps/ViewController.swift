62f57b3d870b379cbb5ec4793391c97f5a370541
Gurinder Hans <hello@gurinderhans.me>
2015-05-30 13:54:32 -0700

    add ios app

---
+++ app/ios/SFUMaps/SFUMaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SFUMaps
+//
+//  Created by Gurinder Hans on 2015-05-30.
+//  Copyright (c) 2015 ghans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
