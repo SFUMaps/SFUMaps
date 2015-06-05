f68f55d20082ed0b5cf4a868283a01916981604e
nbosco <nbosco182@gmail.com>
2015-06-04 19:15:47 -0700

    added zoom limit & trying  map padding

--- app/ios/SFUMaps/SFUMaps/ViewController.swift
+++ app/ios/SFUMaps/SFUMaps/ViewController.swift
@@ -28,14 +28,34 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         super.viewDidLoad()
         var camera = GMSCameraPosition.cameraWithLatitude(49.279014, longitude: -122.916528, zoom: 18.0)
         
+        
         var mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
         self.view = mapView
         
+        //Set max & min zoom levels
+        mapView.setMinZoom(15, maxZoom: 21)
         
+//        //Creates view range coordinates
+//        let left = CLLocationCoordinate2DMake(49.278954, -122.937197)
+//        let right = CLLocationCoordinate2DMake(49.278230, -122.901845)
+//        
+//        //Sets view range coordinates
+//        var bounds = GMSCoordinateBounds(coordinate: left, coordinate: right)
+    
+        
+        var southWest = CLLocationCoordinate2DMake(449.273319,-122.934577);
+        var northEast = CLLocationCoordinate2DMake(49.280993,-122.901603);
+        var overlayBounds = GMSCoordinateBounds(coordinate: southWest, coordinate: northEast)
+        
+        // Image from sfu site
+        var icon = UIImage(named: "file-page1.png")
+        
+        
+        var overlay = GMSGroundOverlay(bounds: overlayBounds, icon: icon)
+        overlay.bearing = 0
+        overlay.map = mapView
         
     }
-    
-    
     
     
     override func didReceiveMemoryWarning() {
