d74a23a9cc6737c33ac11ce0f86b567b9ed4645f
Gurinder Hans <hello@gurinderhans.me>
2015-08-13 17:42:55 -0700

    add TODO note, indent PathMaker.java with tabs

--- app/android/SFUMaps/app/src/main/java/me/gurinderhans/sfumaps/PathMaker/PathMaker.java
+++ app/android/SFUMaps/app/src/main/java/me/gurinderhans/sfumaps/PathMaker/PathMaker.java
@@ -26,93 +26,94 @@ import me.gurinderhans.sfumaps.R;
  */
 public class PathMaker implements MapWrapperLayout.OnDragListener {
 
-    public static final String TAG = PathMaker.class.getSimpleName();
+	public static final String TAG = PathMaker.class.getSimpleName();
 
-    public static final String WALKABLE_KEY = "walkable";
+	public static final String WALKABLE_KEY = "walkable";
 
-    public final GoogleMap mMap;
-    public final MapGrid mMapGrid;
+	public final GoogleMap mMap;
+	public final MapGrid mMapGrid;
 
-    boolean isEditingMap = false;
+	boolean isEditingMap = false;
 
-    public PathMaker(CustomMapFragment mapFragment, GoogleMap map, View editButton, final View exportButton, final MapGrid grid) {
-        this.mMap = map;
-        this.mMapGrid = grid;
+	public PathMaker(CustomMapFragment mapFragment, GoogleMap map, View editButton, final View exportButton, final MapGrid grid) {
+		this.mMap = map;
+		this.mMapGrid = grid;
 
-        mapFragment.setOnDragListener(this);
+		mapFragment.setOnDragListener(this);
 
-        editButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+		editButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
 
-                isEditingMap = !isEditingMap;
+				isEditingMap = !isEditingMap;
 
-                exportButton.setVisibility(isEditingMap ? View.VISIBLE : View.INVISIBLE);
+				exportButton.setVisibility(isEditingMap ? View.VISIBLE : View.INVISIBLE);
 
-                ((ImageButton) v).setImageResource(isEditingMap ? android.R.drawable.ic_menu_close_clear_cancel : android.R.drawable.ic_menu_edit);
+				((ImageButton) v).setImageResource(isEditingMap ? android.R.drawable.ic_menu_close_clear_cancel : android.R.drawable.ic_menu_edit);
 
-                mMap.getUiSettings().setScrollGesturesEnabled(!isEditingMap);
-            }
-        });
+				mMap.getUiSettings().setScrollGesturesEnabled(!isEditingMap);
+			}
+		});
 
-        // export map path
-        exportButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    //
-                    JSONObject root = new JSONObject();
+		// export map path
+		exportButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				try {
+					//
+					JSONObject root = new JSONObject();
 
-                    root.put(WALKABLE_KEY, new JSONArray());
+					root.put(WALKABLE_KEY, new JSONArray());
 
-                    for (int x = 0; x < mMapGrid.mapGridSizeX; x++) {
-                        for (int y = 0; y < mMapGrid.mapGridSizeY; y++) {
-                            if (mMapGrid.getNode(x, y).isWalkable)
-                                root.getJSONArray(WALKABLE_KEY).put(x + "," + y);
-                        }
-                    }
+					for (int x = 0; x < mMapGrid.mapGridSizeX; x++) {
+						for (int y = 0; y < mMapGrid.mapGridSizeY; y++) {
+							if (mMapGrid.getNode(x, y).isWalkable)
+								root.getJSONArray(WALKABLE_KEY).put(x + "," + y);
+						}
+					}
 
-                    // create file
-                    MapTools.createFile("map_grid.json", root.toString());
-                    Toast.makeText(v.getContext(), "Done", Toast.LENGTH_SHORT).show();
+					// create file
+					// TODO: find big area chunks in json to reduce file size
+					MapTools.createFile("map_grid.json", root.toString());
+					Toast.makeText(v.getContext(), "Done", Toast.LENGTH_SHORT).show();
 
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-
-    @Override
-    public void onDrag(MotionEvent motionEvent) {
-        // allow for normal map stuff if not in editing mode
-        if (!isEditingMap)
-            return;
-
-        Point screenPoint = new Point((int) motionEvent.getX(), (int) motionEvent.getY());
-        LatLng latlng = mMap.getProjection().fromScreenLocation(screenPoint);
-        PointF mapPoint = MercatorProjection.fromLatLngToPoint(latlng);
-
-        Point closestPoint = getClosestPoint(mapPoint, mMapGrid);
-        mMapGrid.mMapGrid.get(closestPoint.x).get(closestPoint.y).isWalkable = true;
-
-        // get dist of each point and figure out index of closest point
-        mMap.addMarker(new MarkerOptions()
-                .position(MercatorProjection.fromPointToLatLng(mMapGrid.getNode(closestPoint).node_position))
-                .icon(BitmapDescriptorFactory.fromResource(mMapGrid.getNode(closestPoint).isWalkable ? R.drawable.map_path : R.drawable.no_path))
-                .anchor(0.5f, 0.5f));
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 
-    }
+	@Override
+	public void onDrag(MotionEvent motionEvent) {
+		// allow for normal map stuff if not in editing mode
+		if (!isEditingMap)
+			return;
 
-    public static Point getClosestPoint(PointF point, MapGrid grid) {
+		Point screenPoint = new Point((int) motionEvent.getX(), (int) motionEvent.getY());
+		LatLng latlng = mMap.getProjection().fromScreenLocation(screenPoint);
+		PointF mapPoint = MercatorProjection.fromLatLngToPoint(latlng);
 
-        PointF gridNode = grid.getNode(0, 0).node_position;
+		Point closestPoint = getClosestPoint(mapPoint, mMapGrid);
+		mMapGrid.mMapGrid.get(closestPoint.x).get(closestPoint.y).isWalkable = true;
 
-        // convert dist to grid index and return the position of the node at that index
+		// get dist of each point and figure out index of closest point
+		mMap.addMarker(new MarkerOptions()
+				.position(MercatorProjection.fromPointToLatLng(mMapGrid.getNode(closestPoint).node_position))
+				.icon(BitmapDescriptorFactory.fromResource(mMapGrid.getNode(closestPoint).isWalkable ? R.drawable.map_path : R.drawable.no_path))
+				.anchor(0.5f, 0.5f));
 
-        return new Point((int) ((point.x - gridNode.x) / MapGrid.EACH_POINT_DIST), (int) ((point.y - gridNode.y) / MapGrid.EACH_POINT_DIST));
-    }
+
+	}
+
+	public static Point getClosestPoint(PointF point, MapGrid grid) {
+
+		PointF gridNode = grid.getNode(0, 0).node_position;
+
+		// convert dist to grid index and return the position of the node at that index
+
+		return new Point((int) ((point.x - gridNode.x) / MapGrid.EACH_POINT_DIST), (int) ((point.y - gridNode.y) / MapGrid.EACH_POINT_DIST));
+	}
 
 }
